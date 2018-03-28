9b2d22e8ace71faeb62b99e81f0c3a04b809368c
Michael Täge <mictaege@googlemail.com>
2018-03-28 07:46:36 +0200

    #22 spoonCopyXyz Tasks are never up-to-date during a gradle build

--- src/main/groovy/com/github/mictaege/spoon_gradle_plugin/SpoonTask.groovy
+++ src/main/groovy/com/github/mictaege/spoon_gradle_plugin/SpoonTask.groovy
@@ -13,6 +13,7 @@ import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.TaskExecutionException
+import org.gradle.api.tasks.WorkResult
 import spoon.Launcher
 
 import java.util.function.Function
@@ -23,10 +24,12 @@ class SpoonTask extends DefaultTask {
 
     private Logger log = Logging.getLogger("spoon")
 
-    @InputFiles
     File srcDir
-    @OutputDirectory
     File outDir
+    @InputFiles
+    FileTree javaSrcDir
+    @OutputDirectory
+    File javaOutDir
     Function<File, Boolean> fileFilter
     String[] processors = []
     FileCollection classpath
@@ -35,33 +38,50 @@ class SpoonTask extends DefaultTask {
     @TaskAction
     void run() {
         try {
-            List<String> params = new LinkedList<>()
-
-            addParam(params, '--input', srcDir.getAbsolutePath())
-            addParam(params, '--output', outDir.getAbsolutePath())
-            addParam(params, '--compliance', '' + compliance)
-            if (processors.size() != 0) {
-                addParam(params, '--processors', processors.join(pathSeparator))
-            }
-            if (!classpath.asPath.empty) {
-                addParam(params, '--source-classpath', classpath.asPath)
-            }
-            addKey(params, '--noclasspath')
-            addParam(params, '--level', "OFF")
-            addParam(params, '--output-type', "classes")
-            addKey(params, '--lines')
-            addKey(params, '--disable-model-self-checks')
-
-            addParam(params, '--generate-files', findTypesToSpoon())
-
-            def launcher = new Launcher()
-            String[] args = params.toArray(new String[params.size()])
-            logEnv(args)
-            launcher.setArgs(args)
-            launcher.run()
+            prepareSpoon()
+            runSpoon()
         } catch (final Exception e) {
             throw new TaskExecutionException(this, e)
         }
+    }
+
+    private WorkResult prepareSpoon() {
+        project.copy {
+            from(srcDir.path) {
+                include '**/*.java'
+                exclude { f ->
+                    !f.isDirectory() && fileFilter.apply(f.file)
+                }
+            }
+            into outDir.path
+        }
+    }
+
+    private void runSpoon() {
+        List<String> params = new LinkedList<>()
+
+        addParam(params, '--input', srcDir.getAbsolutePath())
+        addParam(params, '--output', outDir.getAbsolutePath())
+        addParam(params, '--compliance', '' + compliance)
+        if (processors.size() != 0) {
+            addParam(params, '--processors', processors.join(pathSeparator))
+        }
+        if (!classpath.asPath.empty) {
+            addParam(params, '--source-classpath', classpath.asPath)
+        }
+        addKey(params, '--noclasspath')
+        addParam(params, '--level', "OFF")
+        addParam(params, '--output-type', "classes")
+        addKey(params, '--lines')
+        addKey(params, '--disable-model-self-checks')
+
+        addParam(params, '--generate-files', findTypesToSpoon())
+
+        def launcher = new Launcher()
+        String[] args = params.toArray(new String[params.size()])
+        logEnv(args)
+        launcher.setArgs(args)
+        launcher.run()
     }
 
     private logEnv(String[] args) {
