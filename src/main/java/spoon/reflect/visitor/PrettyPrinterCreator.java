e0922ae3e9b1fd42caa2a75ea9194253c6c53c8c
Michael Taege <mictaege@googlemail.com>
2023-07-05 12:14:45 +0200

    Thread-Safe Pretty Printing

--- src/main/java/spoon/reflect/visitor/PrettyPrinterCreator.java
+++ src/main/java/spoon/reflect/visitor/PrettyPrinterCreator.java
@@ -6,14 +6,10 @@ import spoon.compiler.Environment;
 
 public class PrettyPrinterCreator {
 
-    private static DefaultJavaPrettyPrinter printer;
-    
     public static PrettyPrinter createPrettyPrinter(final Environment environment) {
-        if (printer == null) {
-            printer = new DefaultJavaPrettyPrinter(environment);
-            printer.setIgnoreImplicit(false);
-            printer.setPreprocessors(asList(new ForceFullyQualifiedProcessor(), new RemoveAllImportsCleaner()));
-        }
+        final DefaultJavaPrettyPrinter printer = new DefaultJavaPrettyPrinter(environment);
+        printer.setIgnoreImplicit(false);
+        printer.setPreprocessors(asList(new ForceFullyQualifiedProcessor(), new RemoveAllImportsCleaner()));
         return printer;
     }
 }
