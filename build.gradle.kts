76f981f0a8c3bcfdcd23da80a06d22b2f86ef73f
Michael Taege <mictaege@googlemail.com>
2024-06-04 19:59:44 +0200

    updated the javaparser to version 3.26.0 in order to support Java Language Level 21.

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ repositories {
 dependencies {
     implementation(gradleApi())
     implementation("com.google.guava:guava:33.2.0-jre")
-    implementation("com.github.javaparser:javaparser-symbol-solver-core:3.25.10")
+    implementation("com.github.javaparser:javaparser-symbol-solver-core:3.26.0")
     api("fr.inria.gforge.spoon:spoon-core:11.0.0"){
         exclude(group = "org.eclipse.jdt", module = "org.eclipse.jdt.core")
     }
